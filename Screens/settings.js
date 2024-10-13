d22924caf870c82415cb93b55ded86a10041b4f0
xilu56 <xilu56@gmail.com>
2024-10-13 02:53:06 -0700

    Refactored setting.js to toggle theme of the app

--- Screens/settings.js
+++ Screens/settings.js
@@ -1,13 +1,20 @@
 import React, { useContext } from 'react';
 import { View, Text, Button } from 'react-native';
 import { ThemeContext } from '../Context/ThemeContext';
+import colors from '../Helper/colors';
 
 export default function Settings() {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: theme === 'light' ? '#fff' : '#333' }}>
-      <Text style={{ color: theme === 'light' ? '#000' : '#fff' }}>Current Theme: {theme}</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: theme.background,
+      }}
+    >
       <Button title="Toggle Theme" onPress={toggleTheme} />
     </View>
   );
